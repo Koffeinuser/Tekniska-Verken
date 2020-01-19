2404ffaec90bf9d4eb10b2022f5f487eb29e71fb
Christer Sjöberg <christer.sjoberg@mac.com>
2020-01-19 09:47:48 +0100

    Changed lxml to html5lib

--- TekniskaVerken.py
+++ TekniskaVerken.py
@@ -1,12 +1,12 @@
 # App that scrapes a web page and presents the opening hours in Home Assistant
 
-Home Assistant
 import bs4
 import requests
+
 res = requests.get(
     'https://www.tekniskaverken.se/privat/avfall-och-atervinning/atervinningscentralerna/malmen/')
 
-soup = bs4.BeautifulSoup(res.text, 'lxml')
+soup = bs4.BeautifulSoup(res.text, 'html5lib')
 for i in soup.select('.openinghours-status'):
     print(i.text)
     
